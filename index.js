92bdf61e3921f9dd32008cfb35cb1e8bf0887e90
Sevil20 <sevilmirzayeva010@gmail.com>
2022-11-08 02:56:02 +0400

    Done

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ let xbtn = document.querySelector(".x-btn");
 let yellowDiv = document.querySelector('.yellow');
 let container = document.querySelector('.todo-container');
 let xbtn1 = document.querySelector('.x-btn-1');
+let changeBtn = document.querySelector('.success')
+let okeybtn = document.querySelector('.okey1')
+let deletebtn = document.querySelector('.all')
 
 
+
+list.style.backgroundColor = 'white';
 xbtn1.addEventListener('click',(e) =>{
     if(myInput.value===''){
         console.log('hello')
@@ -21,32 +26,39 @@ xbtn1.addEventListener('click',(e) =>{
 })
 list.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
-    e.target.parentElement.remove()
+    changeBtn.style.display = 'block';
+    okeybtn.addEventListener('click',() =>{
+      changeBtn.style.display = 'none';
+      e.target.parentElement.remove();
+      container.style.width = '410px';
+      myInput.style.width = '78%';
+      xbtn1.style.width ='12%';
+      changeBtn.style.width = '95%';
+    })
   }
-});
+ })
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) =>{
   if (myInput.value === "") {
     alert("Deyer daxil et");
   } else {
     list.style.display = "block";
     addList.style.display = "block";
+    list.style.backgroundColor = '#FFDC40';
     let value = myInput.value;
-    list.innerHTML += `<li>${value} <button class="x-btn">X</button></li>`;
+    list.innerHTML += `<li style="padding-top:5px;">${value} <button class="x-btn">X</button></li>`;
     list.style.display = "flex";
     list.style.flexDirection = "column";
     list.style.justifyContent = "space-evenly";
     yellowDiv.style.width = '100%';
-    container.style.width = '470px';
-    myInput.style.width = '410px';
-    list.style.fontSize = '20px';
-    addList.style.borderRadius = '5px';
-    addList.style.borderRadius = 'red';
-    xbtn1.style.display = 'none';
+    container.style.width = '480px';
+    myInput.style.width = '78%';
+    xbtn1.style.width ='12%';
+    changeBtn.style.width = '95%';
     myInput.value = '';
-    if (value.length > 40) {
+    if (value.length > 20) {
       list.innerHTML += `<li>${value} <button class="x-btn">X</button></li>`;
-      list.style.width = "40%";
+      list.style.width = "80%";
     }
   }
 });
@@ -57,10 +69,12 @@ if (
     bottomImgBtn.style.display = "none";
     let arr = [];
   const data = [...list.children];
+    data.shift();
   data.forEach(item =>{
     arr.push(item.innerHTML);
   })
   arr.sort();
+  console.log(arr)
   list.innerHTML = '';
   arr.forEach(item =>{
     let li = `<li>${item}</li>`;
